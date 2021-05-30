6adbbad76bfbb1dc8d292c0f26ba3b1d13b64088
Ievhen Perekhodchenko <i.perekhodchenko@favbet.com>
2021-05-30 16:41:00 +0300

    add rotes

--- src/routes/v1/reportToTm.js
+++ src/routes/v1/reportToTm.js
@@ -3,7 +3,6 @@ const Router = require('@koa/router');
 const { reportControllers } = require('../../controllers');
 
 const router = new Router({ prefix: '/report' });
-
 router.post('report', '/send', reportControllers.send);
 
 module.exports = router;
