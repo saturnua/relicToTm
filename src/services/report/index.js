120c0b736710ca9a850ed321a60856d078aa2e44
Ievhen Perekhodchenko <i.perekhodchenko@favbet.com>
2021-06-01 17:52:44 +0300

    fix time displaying

--- src/services/report/index.js
+++ src/services/report/index.js
@@ -1,24 +1,22 @@
 const bot = require('../../bot');
 
-const {CHAT_ID} = process.env;
+const { CHAT_ID } = process.env;
 
 const send = async (ctx) => {
-    const appName = ctx.metadata ? ctx.metadata['entity.name'] : 'App without name';
-    const error = ctx.targets ? ctx.targets[0]['name'] : 'Error without name :('
-    const {severity} = ctx;
-    const timeOfError = ctx.timestamp_utc_string;
-
-    const message = `
+  const appName = ctx.metadata ? ctx.metadata['entity.name'] : 'App without name';
+  const error = ctx.targets ? ctx.targets[0].name : 'Error without name :(';
+  const { severity } = ctx;
+  const timestamp = ctx.timestamp;
+  const message = `
     🔥
     App "${appName}" has ERROR with severity - "${severity}"\n
-    happens at - ${timeOfError}\n
+    happens at - ${new Date(timestamp).toString()}\n
     ERROR  - "${error}"\n
    ️⛑⛑⛑🔥`;
 
-    await bot.telegram.sendMessage(CHAT_ID, message);
-    return message;
-    // '🔥🔥🔥🔥🔥'
+  await bot.telegram.sendMessage(CHAT_ID, message);
+  return message;
 };
-const reportServices = {send};
+const reportServices = { send };
 
 module.exports = reportServices;
