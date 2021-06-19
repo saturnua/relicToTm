315e98bfeaf6e93d9f7fee1632c689d37f71cd44
Ievhen Perekhodchenko <i.perekhodchenko@favbet.com>
2021-06-19 13:56:03 +0300

    add field to error displaying

--- src/services/report/index.js
+++ src/services/report/index.js
@@ -5,14 +5,20 @@ const { CHAT_ID } = process.env;
 const send = async (ctx) => {
   const appName = ctx.metadata ? ctx.metadata['entity.name'] : 'App without name';
   const error = ctx.targets ? ctx.targets[0].name : 'Error without name :(';
+  const details = ctx.details;
   const { severity } = ctx;
   const timestamp = ctx.timestamp;
+  const policyName = ctx.policy_name;
+  const condition_name = ctx.condition_name;
   const message = `
     🔥
     App "${appName}" has ERROR with severity - "${severity}"\n
     happens at - ${new Date(timestamp).toLocaleString('en-US', { timeZone: 'Europe/Kiev'})}\n
     ERROR  - "${error}"\n
-   ️⛑⛑⛑🔥`;
+    DETAILS - "${details}"\n
+    PolicyName - "${policyName}"\\n
+    ConditionName - "${condition_name}"\\n
+   ️🔥`;
 
   await bot.telegram.sendMessage(CHAT_ID, message);
   return message;
