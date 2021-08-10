ba849c713107b7bb9c843e9b4679734291325a0f
Ievhen Perekhodchenko <i.perekhodchenko@favbet.com>
2021-08-10 23:06:22 +0300

    fix app name

--- src/services/report/index.js
+++ src/services/report/index.js
@@ -3,14 +3,14 @@ const bot = require('../../bot');
 const { CHAT_ID } = process.env;
 
 const send = async (ctx) => {
-  //const appName = ctx.metadata['entity.name'] ? ctx.metadata['entity.name'] : 'App without name';
-  console.log('IN SEND')
+  const appName = ctx.metadata['entity.name'] ? ctx.metadata['entity.name'] : 'App without name';
   const error = ctx.targets ? ctx.targets[0].name : 'Error without name :(';
   const { severity, timestamp, policyName, condition_name, details } = ctx;
 
   const message = `
     🔥
-    App "" has ERROR with severity - "${severity}"\n
+    
+    App "${appName}" has ERROR with severity - "${severity}"\n
     happens at - ${new Date(timestamp).toLocaleString('en-US', { timeZone: 'Europe/Kiev'})}\n
     ERROR  - "${error}"\n
     DETAILS - "${details}"\n
